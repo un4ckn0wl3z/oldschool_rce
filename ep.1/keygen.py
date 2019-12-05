5786b3e37e0a1685c8025fa72373249a1a57ca11
u n 4 c k n 0 w l 3 z <gh0stsync.rt@gmail.com>
2019-12-05 21:31:20 +0700

    Update keygen.py

--- ep.1/keygen.py
+++ ep.1/keygen.py
@@ -1,14 +1,21 @@
 import hashlib
 
+PREFIX = 'iPA'
+CONSTANT_SALT = 'Z'
 
-def insert_salted(string, index):
-    return string[:index] + ' ' + string[index:]
+
+def insert_salt(target_string, index):
+    return target_string[:index] + ' ' + target_string[index:]
 
 
-name = input("Enter Name: ")
+name = input('Enter your name: ')
 md5_name = hashlib.md5(name.encode())
 print("Your hashed: ", md5_name.hexdigest())
 
-salted = str(insert_salted(insert_salted(md5_name.hexdigest(), 2), 4)).replace(' ', 'Z').upper()
-salted = 'iPA' + salted
-print("Your serial: ", hashlib.md5(salted.encode()).hexdigest())
+salted = PREFIX+str(insert_salt(insert_salt(md5_name.hexdigest(), 2), 4).replace(' ', 'Z')).upper()
+
+
+print("Your key: ",  hashlib.md5(salted.encode()).hexdigest())
+
+
+
